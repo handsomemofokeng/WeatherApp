fc0cddc2ef0e84eb54c0f634c54f4e3b172a4073
Handsome Mofokeng <handsome.mofokeng@acumensoft.net>
2022-03-29 01:23:52 +0200

    added CurrentWeather entities

---
+++ app/src/main/java/za/co/digitalhustlers/weatherapp/data/db/unitlocalized/current/UnitSpecificCurrentWeatherEntry.kt
@@ -0,0 +1,13 @@
+package za.co.digitalhustlers.weatherapp.data.db.unitlocalized.current
+
+
+interface UnitSpecificCurrentWeatherEntry {
+    val temperature: Double
+    val conditionText: String
+    val conditionIconUrl: String
+    val windSpeed: Double
+    val windDirection: String
+    val precipitationVolume: Double
+    val feelsLikeTemperature: Double
+    val visibilityDistance: Double
+}
