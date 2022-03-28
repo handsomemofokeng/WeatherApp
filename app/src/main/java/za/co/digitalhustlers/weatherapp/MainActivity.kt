cf89117c5079194e849d18d6031caf0487cb97e1
Handsome Mofokeng <handsome.mofokeng@acumensoft.net>
2022-03-29 01:27:45 +0200

    updated navigation on MainActivity.kt

--- app/src/main/java/za/co/digitalhustlers/weatherapp/MainActivity.kt
+++ app/src/main/java/za/co/digitalhustlers/weatherapp/MainActivity.kt
@@ -26,7 +26,7 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+                R.id.navigation_today, R.id.navigation_this_week, R.id.navigation_settings
             )
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
